cf8929fe7de1ce8e19f6041667f1f78878397596
Praveenkumar Singh <praveenkumar.singh@in.abb.com>
2018-09-20 12:14:14 +0530

    Implemented Logic

--- RnaTranscription.cs
+++ RnaTranscription.cs
@@ -4,6 +4,17 @@ public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] nucleotideRna = nucleotide.ToCharArray();
+        for (int i = 0; i < nucleotideRna.Length; i++)
+        {
+            if (nucleotideRna[i] == 'G')
+                nucleotideRna[i] = 'C';
+            else if (nucleotideRna[i] == 'C')
+                nucleotideRna[i] = 'G';
+            else if (nucleotideRna[i] == 'T')
+                nucleotideRna[i] = 'A';
+            else nucleotideRna[i] = 'U';
+        }
+        return new string(nucleotideRna);
     }
 }
